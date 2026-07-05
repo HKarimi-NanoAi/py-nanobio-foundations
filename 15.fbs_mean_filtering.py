f62ec8daf18bb0259b01dd3de3a1323a8cff3983
HKarimi-NanoAi <h.k.a.r.i.m.iiii1996@gmail.com>
2026-07-05 08:30:37 +0330

    Add mean filtering for fasting blood sugar dataset
    Implement mean filtering on fasting blood sugar dataset to identify values above the cohort average.

---
+++ 15.fbs_mean_filtering.py
@@ -0,0 +1,14 @@
+#Topic: Adaptive Data Filtering and Boolean Masking Based on Dataset Mean
+#Description: Computing the mean of a fasting blood sugar (FBS) dataset and using it as a dynamic threshold to filter out upper values.
+#Application: Clinical screening to identify patients with glucose levels above the cohort average.
+#________________________________________________________________________________________________________________
+
+fblood_sugar_dataset = [95, 120, 88, 140, 105, 110, 92, 130, 85, 115]
+dataset_numpy = np.array(fblood_sugar_dataset)
+
+dataset_mean = np.mean(dataset_numpy)
+filtering = dataset_numpy[dataset_numpy > dataset_mean]
+convert_to_list = filtering.tolist()
+fbs_filtering_str = ','.join(f'{x:.2f}' for x in convert_to_list)
+
+print(f'Patient values higher than mean: {fbs_filtering_str}')
